8706801b2aeb93da81fab81eef4b87a60c140fc0
Filipe Lima <58348420+F-SpaceMan@users.noreply.github.com>
2019-12-04 15:04:02 -0300

    Update conexao.php

--- Oficina2p0/conect/conexao.php
+++ Oficina2p0/conect/conexao.php
@@ -1,3 +1,7 @@
 <?php
      $conexao = mysqli_connect("localhost:3308", "root", "", "oficina") or die("FALHA NA CONEXÃO: " . mysqli_error());
+     /*Este arquivo é responsável pela conexão com o banco de dados. Verifique se a porta, o usuário e a senha
+     são os mesmos para a sua máquina.
+     */
+?>
 
